3fa543820bb51d855a6ffdc5ba29eca41838d970
oskar046 <oskar046@yandex.com>
2016-05-25 06:32:27 +0200

    Fix android application name in libcocos2dx project

--- cocos2d/cocos/platform/android/libcocos2dx/build/generated/source/aidl/release/com/enhance/gameservice/IGameTuningService.java
+++ cocos2d/cocos/platform/android/libcocos2dx/build/generated/source/aidl/release/com/enhance/gameservice/IGameTuningService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Volumes/Work/MatchEmUp/MachEmUp/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+ * Original file: /Volumes/Work/MatchEmUp/MatchEmUp/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
  */
 package com.enhance.gameservice;
 public interface IGameTuningService extends android.os.IInterface
